7d428c73b96220f3f3d300738381e29e42076e3b
Bentechy66 <sendbenspam@yahoo.co.uk>
2017-09-20 13:10:13 +0100

    Create about command

--- ILBot.py
+++ ILBot.py
@@ -17,5 +17,18 @@ bot = botobject.bot #import the bot object
 
 bot.loop.create_task(online.CheckOnlineUsers()) #create coroutine
 
+@bot.command()
+async def about(Aliases:["credits"]):
+    msgc = await bot.say("**ABOUT / Credits**\nThis bot shows local time in specific timezones and also handles points.\nWritten by BenTechy66 on Discord.\n\n**Credits**\nBen - *wrote the bot*\nMcTrees - *hosting the bot*\nOpen Source Libraries:Discord.py, asyncio, pytz.")
+    await bot.add_reaction(msgc, "tick:326377249223999498")
+  
+@bot.event
+async def on_reaction_add(reaction, user):
+    if reaction.me == False:
+        await bot.send_message(reaction.message.channel, "you reacted!")
+    await bot.send_message(reaction.message.channel, "Reaction.me status @ discord.Object:0x3326f4 " + str(reaction.me))
     
+
+
+   
 bot.run(credentials.BotSecret) #run bot
