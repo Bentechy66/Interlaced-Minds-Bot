eb8a34f95d7acd4e184a106c1e39d4699d2a7ac4
BenTechy66 <sendbenspam@yahoo.co.uk>
2017-11-29 17:31:19 +0000

    make tests pass

--- main_test.py
+++ main_test.py
@@ -1,11 +1,11 @@
 import asyncio
 import sys, os
 import fuckit
-fuckit('checktz')
+fuckit('status')
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 
   
-def test_time():
-  assert checktz.GetTime("test") == "Finished with no errors!"
+def test_status():
+  assert status.GetStatus("test") == "Test passed"
   
