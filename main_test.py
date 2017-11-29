3d13d7a0f301c5d4e0b187dbad3ecd1d3caa12b4
BenTechy66 <sendbenspam@yahoo.co.uk>
2017-11-29 14:32:04 +0000

    Add test cases
    cba m8

--- main_test.py
+++ main_test.py
@@ -1,6 +1,5 @@
-def fun(c):
-  return c + 1;
+import points
+
   
-def test_a():
-  assert fun(4) == 5
-  assert fun(4) == 6
+def test_points():
+  assert await points.AddR() == "Finished with no errors!"
